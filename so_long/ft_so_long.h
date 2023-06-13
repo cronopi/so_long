f9dfe39e3d15d06ca0cec9825c0b0084420a9888
cronopi <99333081+cronopi@users.noreply.github.com>
2023-06-13 10:34:01 +0200

    Add files via upload

---
+++ so_long/ft_so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FT_so_long.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcastano <rcastano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 09:01:09 by rcastano          #+#    #+#             */
+/*   Updated: 2023/06/13 09:22:53 by rcastano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include FT_SO_LONG_H
+# define FT_PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <math.h>
+# include "libft/libft.h"
+
+#endif
