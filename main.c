83aba03ed106415cb9f34197c87757d084913903
Roberto Castano Blanco <rcastano@c1r3s5.42madrid.com>
2023-06-13 10:56:54 +0200

    ventana

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
-#include "so_long.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcastano <rcastano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 10:40:55 by rcastano          #+#    #+#             */
+/*   Updated: 2023/06/13 10:55:48 by rcastano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+//# include "so_long.h"
+# include "mlx.h"
+
+int main(void)
 {
+	void *mlx;
+	void *window;
 
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, 1000, 1000, "patata");
+
+	mlx_loop(mlx);
 	return (0);
 }
